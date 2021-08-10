541305eb962382dbc5bd681dafb79baf2ff3f450
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-10 15:58:37 +0530

    Added: check it's tie then game continues till the difference of minimum

--- Simulation.sh
+++ Simulation.sh
@@ -4,28 +4,29 @@ head=0
 tail=0
 while [ $head -le 21 ] && [ $tail -le 21 ]
 do
-    flip=$(($RANDOM%2))
-     if [ $flip -eq 1 ]
-     then
-         head=$(($head+1))
-      else
-         tail=$(($tail+1))
-      fi
-       if [ $head -eq $tail ]
-       then
-         echo "match tie"
-       else
-	echo "No.of heads= $head"
-	echo "No.of tails= $tail"
-	  if [ $head -gt $tail ]
-	  then
-	     echo "head won"
-	         win=$(($head-$tail))
-	     echo "head won by $win"
-	  else
-	     echo "tail won"
-		 win=$(($tail-$head))
-	     echo "tail won by $win"
-	  fi
-	fi
+        flip=$(($RANDOM%2))
+        if [ $flip -eq 1 ]
+        then
+                head=$(($head+1))
+        else
+                tail=$(($tail+1))
+        fi
+		if [ $head -eq $tail ]
+		then
+			echo "match tie"
+			continue
+		else
+			echo "No.of heads= $head"
+			echo "No.of tails= $tail"
+			if [ $head -gt $tail ]
+			then
+				echo "head won"
+				win=$(($head-$tail))
+				echo "head won by $win"
+			else
+				echo "tail won"
+				win=$(($tail-$head))
+				echo "tail won by $win"
+			fi
+		fi
 done
